c5a860be716da3217def55393769b34dcec45a86
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-05-16 15:40:32 -0300

    feat: adjust SRTN. RR for arrival with variation

--- main.py
+++ main.py
@@ -18,66 +18,85 @@ class Task:
 
 
 class EscalatorSRTN:
-    def __init__(self, tasks, static_tasks, X):
-        self.tasks: [Task] = tasks
+    def __init__(self, static_tasks, X):
+        self.tasks: [Task] = []
         self.static_tasks: [Task] = static_tasks
         self.total_time = X
-        self.current_task: Task = self.tasks[0]
+        self.current_task: Task = None
 
     def execute(self):
+        print("\n ================\n SRTN \n ================ \n")
         for i in range(0, self.total_time, 1):
-            print("Task: {} || Time: {} || Value: {}".format(self.current_task.name, i, self.current_task.time_process))
+            if self.current_task:
+                print("Task: {} || Time: {} || Value: {}".format(self.current_task.name, i,
+                                                                 self.current_task.time_process))
             self.kill_task()
             self.verify_arrival(i)
             self.preempt()
             self.current_task.execute()
-            self.tasks.sort(key=sort_func)
 
     def preempt(self):
-        self.tasks.sort(key=sort_func)
-        self.current_task = self.tasks[0]
+        if len(self.tasks) != 0:
+            self.tasks.sort(key=sort_func)
+            self.current_task = self.tasks[0]
 
     def kill_task(self):
-        for task in self.tasks:
-            if task.time_process == 0:
-                self.tasks.remove(task)
+        if len(self.tasks) != 0:
+            for task in self.tasks:
+                if task.time_process == 0:
+                    self.tasks.remove(task)
 
     def verify_arrival(self, current_time):
         for task in self.static_tasks:
+            if task.arrival == current_time:
+                self.tasks.append(
+                    Task(task.period, task.time_process, task.arrival, task.name + "-{}".format(current_time)))
             if (current_time % task.period) == 0 and current_time != 0:
                 self.tasks.append(
                     Task(task.period, task.time_process, task.arrival, task.name + "-{}".format(current_time)))
 
 
 class EscalatorRR:
-    def __init__(self, tasks, static_tasks, X):
-        self.tasks: [Task] = tasks
+    def __init__(self, static_tasks, X):
+        self.tasks: [Task] = []
         self.static_tasks: [Task] = static_tasks
         self.total_time = X
-        self.current_task: Task = self.tasks[0]
+        self.current_task: Task = None
 
     def execute(self):
+        print("\n ================\n ROUND ROBIN \n ================ \n")
         for i in range(0, self.total_time, 1):
-            print("Task: {} || Time: {} || Value: {}".format(self.current_task.name, i, self.current_task.time_process))
+            if self.current_task:
+                print("Task: {} || Time: {} || Value: {}".format(self.current_task.name, i,
+                                                                 self.current_task.time_process))
+
             self.kill_task()
-            self.current_task = self.tasks[0]
-            self.preempt()
             self.verify_arrival(i)
-            self.current_task.execute()
+            if len(self.tasks) != 0:
+                self.current_task = self.tasks[0]
+            self.preempt()
+            if self.current_task is not None:
+                self.current_task.execute()
 
     def preempt(self):
-        if self.current_task.quantum_execute_cycle_time == self.current_task.quantum:
-            self.tasks[0].quantum_execute_cycle_time = 0
-            self.tasks.append(self.tasks.pop(0))
-            self.current_task = self.tasks[0]
+        if len(self.tasks) != 0:
+            if self.current_task.quantum_execute_cycle_time == self.current_task.quantum:
+                self.tasks[0].quantum_execute_cycle_time = 0
+                self.tasks.append(self.tasks.pop(0))
+                self.current_task = self.tasks[0]
 
     def kill_task(self):
-        for task in self.tasks:
-            if task.time_process == 0:
-                self.tasks.remove(task)
+        if len(self.tasks) != 0:
+            for task in self.tasks:
+                if task.time_process == 0:
+                    self.tasks.remove(task)
 
     def verify_arrival(self, current_time):
         for task in self.static_tasks:
+            if task.arrival == current_time:
+                self.tasks.append(
+                    Task(task.period, task.time_process, task.arrival, task.name + "-{}".format(current_time),
+                         task.quantum))
             if (current_time % task.period) == 0 and current_time != 0:
                 self.tasks.append(
                     Task(task.period, task.time_process, task.arrival, task.name + "-{}".format(current_time),
@@ -87,14 +106,14 @@ class EscalatorRR:
 def main():
     X = int(input('Qual a duração: '))
 
-    tasks = [Task(10, 4, 0, 'A', 5), Task(20, 8, 0, 'B', 5), Task(30, 12, 0, 'C', 5)]
+    tasks = []  # [Task(10, 4, 0, 'A', 5), Task(20, 8, 0, 'B', 5), Task(30, 12, 0, 'C', 5)]
     static_tasks = [Task(10, 4, 0, 'A', 5), Task(20, 8, 0, 'B', 5), Task(30, 12, 0, 'C', 5)]
 
     # SRTN
-    # escalator = EscalatorSRTN(tasks, static_tasks, X)
-    # escalator.execute()
+    escalator = EscalatorSRTN(static_tasks, X)
+    escalator.execute()
     # ROUND ROBIN
-    escalator = EscalatorRR(tasks, static_tasks, X)
+    escalator = EscalatorRR(static_tasks, X)
     escalator.execute()
 
 
